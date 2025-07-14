2441fe62dc9cbb28ededa5f3465ce860d5d7e880
pikimen357 <vidkyadhipradana2004@mail.ugm.ac.id>
2025-07-14 08:42:40 +0700

    routing

--- routes/web.php
+++ routes/web.php
@@ -7,5 +7,51 @@ Route::get('/', function () {
 });
 
 Route::get('/hello', function () {
-    return "<h1>Hello World</h1>";
+    return "<h1>Halo Dunia</h1>";
 });
+
+$movies = [];
+
+for($i = 0; $i <= 3; $i++) {
+        $movies[] = [
+            'title' => 'Movie ' . $i + 1,
+            'year' => "2022",
+            'genre' => 'Action'
+        ];
+}
+
+Route::get('/movie', function () use ($movies) {
+    return $movies;
+});
+
+Route::post('/movie', function () use ($movies){
+    $movies[] = [
+        'title' => request('title'),
+        'year' => request('year'),
+        'genre' => request('genre')
+    ];
+
+    return $movies;
+});
+
+Route::put('/movie/{id}', function ($id) use ($movies){
+    $movies[$id-1]['title'] = request('title');
+    $movies[$id-1]['year'] = request('year');
+    $movies[$id-1]['genre'] = request('genre');
+
+    return $movies;
+});
+
+Route::patch('/movie/{id}', function ($id) use ($movies){
+    $movies[$id-1]['title'] = request('title');
+    $movies[$id-1]['year'] = request('year');
+
+    return $movies;
+});
+
+Route::delete('/movie/{id}', function ($id) use ($movies){
+    unset($movies[$id-1]);
+
+    return $movies;
+});
+
---
+++ tests/Feature/RoutingTest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class RoutingTest extends TestCase
+{
+
+    public function testRoute()
+    {
+        $response = $this->get('/hello');
+        $response->assertSeeText('Halo Dunia');
+    }
+
+    public function testRouteParameterGet(){
+        $response = $this->get('/movie/1');
+        $response->assertSeeText('2022');
+    }
+
+    public function testRouteParameterPut()
+    {
+        $response = $this->put('/movie/1', [
+            'title' => 'Test',
+            'year' => '2022',
+            'genre' => 'Animal',
+        ]);
+
+        $response->assertSeeText('Test')
+                ->assertDontSeeText('Movie 1')
+                ->assertSeeText('Animal');
+    }
+
+    public function testRouteParameterPatch()
+    {
+        $response = $this->patch('/movie/2', [
+            'title' => 'Test',
+            'year' => '2023',
+        ]);
+
+        $response->assertSeeText('Test')
+        ->assertDontSeeText('Movie 2')
+        ->assertSeeText('2023');
+    }
+
+    public function testRouteParameterDelete(){
+        $response = $this->delete('/movie/2');
+        $response->assertDontSeeText('Movie 2');
+    }
+}
